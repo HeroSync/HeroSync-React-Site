eeba10a9465efe0fd1481658db5569cbd3a668c6
Alex <tcbskater@hotmail.com>
2021-04-20 00:01:52 -0400

    Updates

---
+++ precache-manifest.c28315224fcfecddc7523dfd645d14ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbf91ed8850ccc0d4cc8b4d3453aa63",
+    "url": "/HeroSync-React-Site/index.html"
+  },
+  {
+    "revision": "6c12b1f19c87458a9dd7",
+    "url": "/HeroSync-React-Site/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "3676e0bba3d08bca69fa",
+    "url": "/HeroSync-React-Site/static/css/main.b8b3efe6.chunk.css"
+  },
+  {
+    "revision": "6c12b1f19c87458a9dd7",
+    "url": "/HeroSync-React-Site/static/js/2.c1a30651.chunk.js"
+  },
+  {
+    "revision": "dba6c7cbf3e2c690119a165676f203c3",
+    "url": "/HeroSync-React-Site/static/js/2.c1a30651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3676e0bba3d08bca69fa",
+    "url": "/HeroSync-React-Site/static/js/main.2887cc02.chunk.js"
+  },
+  {
+    "revision": "56b4861f8539ae33a41e",
+    "url": "/HeroSync-React-Site/static/js/runtime-main.66bafb95.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hs-react/precache-manifest.b57264d1204fae216dae58aa2222f2e5.js"
+  "/HeroSync-React-Site/precache-manifest.c28315224fcfecddc7523dfd645d14ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hs-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HeroSync-React-Site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
